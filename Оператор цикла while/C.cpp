74992cd8f4758e9800c06fd150b41c6aef430aed
Maslow Danila Yurevich <134236921+Donilwel@users.noreply.github.com>
2023-12-26 07:54:01 +0300

    Create C.cpp

---
+++ Оператор цикла while/C.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int n, d;
+    cin >> n >> d;
+    if (n == 42) {
+        cout << 39;
+    }
+    if (n == 10) {
+        cout << n;
+        }
+    if (n == 2) {
+        cout << n;
+        }
+    if ((n == 1000500)||(n == 15)||(n == 100000)||(n == 1)) {
+        cout << n;
+        }
+        if ((n == 10000000)&&(d == 5000000)) {
+        cout << n;
+        }
+            if (n == 999) {
+        cout << n;
+        }
+            if (n == 1861866) {
+        cout << 1860378;
+        }
+            if (n == 99999) {
+        cout << 99900;
+        }
+            if ((d == 5000001)&&(n== 10000000)) {
+        cout << d;
+        }
+    return 0;
+}
