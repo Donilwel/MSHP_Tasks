5c665322996e6da55bdea7a17fbc766b6c949ac4
Maslow Danila Yurevich <134236921+Donilwel@users.noreply.github.com>
2023-12-26 07:50:23 +0300

    Create B.cpp

---
+++ Оператор switch и тернарная операция/B.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int name;
+    cin >> name;
+switch (name) {
+    case 1: cout << "Cyclop"; break;
+    case 2: cout << "Earthman"; break;
+    default: cout << "Multieye";
+}
+        return 0;
+}
