135e86f2d8f46b8b330b2dbb06e4bd115d3b8cf2
Maslow Danila Yurevich <134236921+Donilwel@users.noreply.github.com>
2023-12-26 07:53:24 +0300

    Create A.cpp

---
+++ Оператор цикла while/A.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main() 
+{ 
+int i = 1, n; 
+    cin >> n;
+while (i <= n) { 
+cout << "Programming School is the best!" << endl; 
+i = i +1; 
+} 
+return 0; 
+}
