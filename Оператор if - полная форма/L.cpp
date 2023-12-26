72b987a2aa27cbe452984ffab5782e5b97cdfc22
Maslow Danila Yurevich <134236921+Donilwel@users.noreply.github.com>
2023-12-26 07:40:10 +0300

    Create L.cpp

---
+++ Оператор if - полная форма/L.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(){
+    short int x,y;
+   cin>>x>>y;
+ 
+   if(( y > 0.5 && y <1.5) || (x > 2))
+ 
+     cout<<1;
+else
+     cout<<0;
+ 
+    return 0;
+}
