bca204be8515f8e07e8cedbf5615a6a291e0a9e5
Maslow Danila Yurevich <134236921+Donilwel@users.noreply.github.com>
2023-12-26 07:50:56 +0300

    Create D.cpp

---
+++ Оператор switch и тернарная операция/D.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int place;
+    cin >> place;
+switch (place) {
+    case 1: cout << "MONDAY"; break;
+    case 2: cout << "TUESDAY"; break;
+    case 3: cout << "WEDNESDAY"; break;
+    case 4: cout << "THURSDAY"; break;
+    case 5: cout << "FRIDAY"; break;
+    case 6: cout << "SATURDAY"; break;
+    case 7: cout << "SUNDAY"; break;
+}
+        return 0;
+}
