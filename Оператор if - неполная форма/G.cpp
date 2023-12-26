a1ed746a5b3bbd59c028b5ca8cd6f77b5245a6bb
Maslow Danila Yurevich <134236921+Donilwel@users.noreply.github.com>
2023-12-26 07:33:52 +0300

    Create G.cpp

---
+++ Оператор if - неполная форма/G.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a >= 0){
+        a > 0;
+    }
+    if (b >= 0) {
+        b > 0;
+    }
+    if (c >= 0) {
+        c > 0;
+    }
+    if (a < 0) {
+        a = 0;
+    }
+    if (b < 0) {
+        b = 0;
+    }
+    if (c < 0) {
+        c = 0;
+    }
+    cout << (a + b + c);
+    return 0;
+}
