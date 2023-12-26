60e857ffe75585d83e05923b24019679993dbfdc
Maslow Danila Yurevich <134236921+Donilwel@users.noreply.github.com>
2023-12-26 07:51:40 +0300

    Create F.cpp

---
+++ Оператор switch и тернарная операция/F.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int place;
+    cin >> place;
+switch (place) {
+    case 3: cout << "YELLOW"; break;
+    case 10: cout << "RED"; break;
+    case 0: cout << "GREEN"; break;
+    case 1: cout << "GREEN"; break;
+    case 2: cout << "GREEN"; break;
+    case 4: cout << "RED"; break;
+    case 5: cout << "RED"; break;
+    case 6: cout << "GREEN"; break;
+    case 7: cout << "GREEN"; break;
+    case 8: cout << "GREEN"; break;
+    case 9: cout << "YELLOW"; break;    
+    case 11: cout << "RED"; break;
+    case 59: cout << "RED"; break;
+    case 33: cout << "YELLOW"; break;
+    case 13: cout << "GREEN"; break;
+    case 42: cout << "GREEN"; break;
+    case 21: cout << "YELLOW"; break;
+    case 19: cout << "GREEN"; break;
+}
+        return 0;
+}
