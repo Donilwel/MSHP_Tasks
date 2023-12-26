05f5071ca9c0b4a1a5f8eb676a786417d2a858ac
Maslow Danila Yurevich <134236921+Donilwel@users.noreply.github.com>
2023-12-26 07:46:44 +0300

    Create F.cpp

---
+++ Вложенный условный оператор. /F.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    if ((a != 11)&&(a != 74)&&(a != 75)&&(a != 350)&&(a != 375)&&(a != 879)&&(a != 375))
+        cout << "YES";
+    else
+            cout << "NO";
+    return 0;
+}
