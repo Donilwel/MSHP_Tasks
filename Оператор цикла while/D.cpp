7afb1fd411181dcc3f0d64a176c71d52c05473a9
Maslow Danila Yurevich <134236921+Donilwel@users.noreply.github.com>
2023-12-26 07:54:22 +0300

    Create D.cpp

---
+++ Оператор цикла while/D.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int x;
+
+   int i = 0;
+
+   while (i < 10) {
+
+     cin >> x;
+
+     cout << (2 * x * x) << endl;
+
+     ++i;
+
+   }
+
+   return 0;
+
+}
