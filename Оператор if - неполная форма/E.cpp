0c4abad21201a3f364687202d150f80c1474d122
Maslow Danila Yurevich <134236921+Donilwel@users.noreply.github.com>
2023-12-26 07:33:13 +0300

    Create E.cpp

---
+++ Оператор if - неполная форма/E.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main() {
+    int n, m;
+    cin >> n >> m;
+    if (abs(n) > abs(m)){
+        n -= 100;
+    }
+    cout << n << " " << m << " ";
+    return 0;
+}
