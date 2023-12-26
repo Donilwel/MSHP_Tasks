fdfa24fc306bce7f6fed6ebe0ab2803cf716a818
Maslow Danila Yurevich <134236921+Donilwel@users.noreply.github.com>
2023-12-26 07:51:12 +0300

    Create E.cpp

---
+++ Оператор switch и тернарная операция/E.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int place;
+    cin >> place;
+switch (place) {
+    case 1: cout << "WINTER"; break;
+    case 2: cout << "WINTER"; break;
+    case 3: cout << "SPRING"; break;
+    case 4: cout << "SPRING"; break;
+    case 5: cout << "SPRING"; break;
+    case 6: cout << "SUMMER"; break;
+    case 7: cout << "SUMMER"; break;
+    case 8: cout << "SUMMER"; break;
+    case 9: cout << "AUTUMN"; break;
+    case 10: cout << "AUTUMN"; break;
+    case 11: cout << "AUTUMN"; break;
+    case 12: cout << "WINTER"; break;
+}
+        return 0;
+}
