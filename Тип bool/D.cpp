c3d24a91cedc9ad50e082e13837758de2b89ba6e
Maslow Danila Yurevich <134236921+Donilwel@users.noreply.github.com>
2023-12-26 07:29:48 +0300

    Create D.cpp

---
+++ Тип bool/D.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    if (x < 150){
+        cout << 0;
+    }
+    else {
+        cout << 1;
+    }
+    return 0;
+}
