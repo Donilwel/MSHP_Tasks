fcadee4a1c8ac4e241ecfd66becf1fbe5362b78b
Maslow Danila Yurevich <134236921+Donilwel@users.noreply.github.com>
2023-12-26 07:37:44 +0300

    Create E.cpp

---
+++ Оператор if - полная форма/E.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x, a, b;
+    cin >> x;
+    if (x >= - 4 && x <=1) {
+        cout << 1;
+    }
+    else {
+        x = 0;
+        cout << 0;
+    }
+    return 0;
+}
