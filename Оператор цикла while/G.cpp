583b1481bfa2e2088c14affb9a963e4c7cbd9f62
Maslow Danila Yurevich <134236921+Donilwel@users.noreply.github.com>
2023-12-26 07:55:10 +0300

    Create G.cpp

---
+++ Оператор цикла while/G.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int a , b;
+    cin >> a >> b;
+   if(a < b) {
+       while (a <= b) {
+        cout << a << endl;
+          a++;
+      }
+   }else {
+       while (a >= b) {
+           cout << a << endl;
+           a--;
+       }
+   }
+   return 0;
+}
